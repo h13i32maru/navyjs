cabf406693a9778850c6d47a2949f86dfbf43044
h13i32maru <h13i32maru@gmail.com>
2015-01-04 00:39:20 +0900

    feature(async): add async function and test.

--- framework/src/lib/async.js
+++ framework/src/lib/async.js
@@ -1,3 +1,38 @@
 export default function async(generatorFunction) {
-  generatorFunction();
+  var gen = generatorFunction();
+
+  return new Promise(function(resolve, reject){
+    onFulfilled();
+
+    function onFulfilled(val) {
+      try {
+        var result = gen.next(val);
+      } catch (e) {
+        return reject(e);
+      }
+      return chain(result);
+    }
+
+    function onRejected(e) {
+      try {
+        var result = gen.throw(e);
+      } catch (e) {
+        return reject(e);
+      }
+      return chain(result);
+    }
+
+    function chain(result) {
+      if (result.done) {
+        return resolve(result.value);
+      }
+
+      var promise = result.value;
+      if (promise instanceof Promise) {
+        return promise.then(onFulfilled).catch(onRejected);
+      } else {
+        reject(new Error('should be promise'));
+      }
+    }
+  });
 }
--- framework/test/lib/async_test.js
+++ framework/test/lib/async_test.js
@@ -1,7 +1,74 @@
 let async = System.get('../src/lib/async').default;
 
 describe('async: ', ()=>{
-  it('is function', ()=>{
-    assert.equal(typeof async, 'function');
+  var VALUE = 123;
+
+  function delayValue(value, msec) {
+    return new Promise((resolve, reject)=>{
+      if (msec < 0) {
+        return reject(new Error('msec must be greater than 0'));
+      }
+
+      setTimeout(()=>{
+        resolve(value);
+      }, msec);
+    });
+  }
+
+  it('executes an async function as sync function.', (done)=>{
+    async(function*(){
+      var x = yield delayValue(VALUE, 100);
+      assert.equal(x, VALUE);
+
+      var y = yield delayValue(VALUE * 2, 200);
+      assert.equal(y, VALUE * 2);
+
+      return x;
+    }).then((value)=>{
+      assert.equal(value, VALUE);
+      done();
+    });
+  });
+
+  it('throws exception if an async function throws exception.', (done)=>{
+    async(function*(){
+      try {
+        var x = yield delayValue(VALUE, -1);
+        assert(false);
+      } catch (e) {
+        assert(e instanceof Error);
+      }
+
+      var y = yield delayValue(VALUE * 2, 100);
+      assert.equal(y, VALUE * 2);
+      done();
+    });
+  });
+
+  it('is rejected if exception is thrown.', (done)=>{
+    async(function*(){
+      throw new Error();
+    }).catch((e)=>{
+      assert(e instanceof Error);
+      done();
+    });
+  });
+
+  it('is rejected if an async function throws unhandled exception.', (done)=>{
+    async(function*(){
+      var x = yield delayValue(VALUE, -1);
+    }).catch((e)=>{
+      assert(e instanceof Error);
+      done();
+    });
+  });
+
+  it('is rejected if an async function returns non promise.', (done)=>{
+    async(function*(){
+      yield setTimeout(()=>{}, 100);
+    }).catch((e)=>{
+      assert(e instanceof Error);
+      done();
+    });
   });
 });
