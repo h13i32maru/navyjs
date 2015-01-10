84b3f914bcdd1ec421ed01ea56889a2547f3efbe
h13i32maru <h13i32maru@gmail.com>
2015-01-10 21:16:18 +0900

    travis: retry.

--- framework/script/test.sh
+++ framework/script/test.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+# usage: test.sh [travis|safari]
 
 ./script/build.sh
 ./script/build_test.sh
 
-case "$1" in
+type=$1
+
+# npm version of travis is 1.4.
+# npm@1.4 can not use `npm run test -- args`
+# so need to use env variable.
+if [ -n "$TRAVIS" ]
+then
+    type="travis"
+fi
+
+case "$type" in
     "travis" )
         ./node_modules/.bin/karma start test/karma.conf.js --browsers ChromeTravis
         ;;
