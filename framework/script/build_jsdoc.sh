9ee42c02017e6bd3e42e5f287564ad95cc3c36d4
h13i32maru <h13i32maru@gmail.com>
2015-01-12 12:44:16 +0900

    f/chore(jsdoc): use 6to5 to build jsdoc.
    6to5 keeps comment(jsdoc), but traceur does not keep comment.

--- framework/script/build_jsdoc.sh
+++ framework/script/build_jsdoc.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
 mkdir -p tmp
-cp -af src tmp/
 
-for file in $(find src -name '*.js')
-do
-    # remove unsupported ES6 feature with node.
-    cat $file |
-     sed 's/export\( default\)*//' | # 'export default' => ''
-     sed 's/\(([^()]*)\) *=>/function\1/' | # '(a, b)=>' => 'function(a, b)'
-     sed 's/^\( *import\)/\/\/\1/' > tmp/$file # 'import a from "a"' => '//import a from "a"'
-done
-
-node --harmony ./node_modules/.bin/jsdoc -c jsdoc.json -r tmp/src -d doc
+./node_modules/.bin/6to5 src -d tmp/src
+./node_modules/.bin/jsdoc -c jsdoc.json -r tmp/src -d doc
 
 rm -rf tmp/src
 
